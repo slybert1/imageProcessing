09f424259138d53b4836f644c9e5e20e60f01068
slybert1 <nairoroad@gmail.com>
2016-06-15 17:01:28 +0000

    Add files via upload

---
+++ imageProcess_6_15_12:00.py
@@ -0,0 +1,147 @@
+import csv
+import subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import interpolate
+from scipy import ndimage
+from scipy.optimize import curve_fit
+from scipy.stats import norm
+from scipy import optimize
+from numpy import *
+from pylab import *
+
+np.set_printoptions(threshold=np.inf)
+
+#Call the camera to take and save an image
+#subprocess.call(["./../flycapture2-2.7.3.19-amd64/flycapture/bin/Triggering"])
+
+#Converts the .pgm file to .bmp
+#img = Image.open("TestImage.pgm")
+
+#img = img.save('result.bmp')
+
+#Converts the bitmap to an n-deminsional array.
+im = Image.open("result0.bmp")
+
+arr = np.array(im)
+subArr = arr
+x, y  = subArr.shape
+
+#-------------------------------------------------------
+#the following subtracts the background from the picture:
+
+for i in range(0, x):
+	for ii in range(0, y):
+		if subArr[i][ii] < 120:
+			subArr[i][ii] = 0
+
+#-------------------------------------------------------
+#The following  finds the center of mass of the array:
+Y,X = ndimage.measurements.center_of_mass(subArr)
+
+#arr is the original image array, subArr the original with a subtracted background
+
+#-------------------------------------------------------
+#calculate the bounds for the small array
+
+lowerX = int(X) - 75
+upperX = int(X) + 75
+lowerY = int(Y) - 75
+upperY = int(Y) + 75
+
+smallArray = np.zeros((151,151))
+
+#now make the picture small:
+m = 0
+n = 0
+
+for j in range(lowerY, upperY):
+        for jj in range(lowerX, upperX):
+                smallArray[m][n] = arr[j][jj]
+		n = n+1
+	m = m+1
+	n = 0
+#-------------------------------------------------------
+
+#creating the gaussian fit with the small array:
+
+#sum of each column into rows:
+xArray = np.zeros(151)
+
+for i in range(0, 150):
+	xArray[i] = sum(smallArray[:,i])
+
+#sum of each row into columns:
+yArray =  np.zeros(151)
+
+for i in range(0,150):
+	yArray[i] = sum(smallArray[i,:])
+
+#function for the gaussian:
+
+def gaussFit(arr, a, mu, sigma, b):
+	return a*np.exp(-(arr-mu)**2/(2*sigma**2)) + b
+
+xAxis = np.linspace(-75, 75, 151)
+
+#fitting the gaussian:
+popt, pcov = curve_fit(gaussFit, xAxis, xArray)
+
+px = gaussFit(xAxis, popt[0], popt[1], popt[2], popt[3])
+
+#plot of x
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#ax.plot(xAxis, px, c = 'r', label = 'Fit')
+#ax.scatter(xAxis, xArray)
+#plt.show()
+
+popty, pcovy = curve_fit(gaussFit, xAxis, yArray)
+
+py = gaussFit(xAxis, popty[0], popty[1], popty[2], popty[3])
+
+#plot of Y
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#ax.plot(xAxis, py, c = 'r', label = 'Fit')
+#ax.scatter(xAxis, yArray)
+#plt.show()
+
+p, q = ndimage.measurements.center_of_mass(smallArray)
+
+#g = px.argmax()
+#h = py.argmax()
+
+#must smooth the gaussian fit in order to find the correct maximum:
+
+#function to interpolate the data:
+f = interpolate.interp1d(xAxis, px)
+
+#resample with more samples:
+xx = np.linspace(-75, 75, 10000)
+
+#compute the smoothed function:
+pxx = f(xx)
+
+g = np.argmax(pxx)
+
+print(g)
+#window = scipy.signal.gaussian(200, 150)
+
+
+print(q, p)
+#print(g, h)
+
+#-------------------------------------------------------
+
+#err = open("cameraDetect.txt", 'r')
+#err = float(err.read())
+#print("center:", X, Y)
+#print(err)
+
+#data = open("errorfile.csv", "a")
+#w = csv.writer(data)
+#w.writerow( (X,Y,err) )
+#data.close()
